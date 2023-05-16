4a8ed97ff4c574c270dc49af0bec0d52c290c84c
Anna Ligon <anna.ligon@hfg-gmuend.de>
2023-05-16 20:33:28 +0200

    clear all function & style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     let todoText = '';
     let todos = [];
+    let deletedTodos = [];
 
     function saveTodoList(todoList) {
                 localStorage.setItem('todoList', JSON.stringify(todoList));
@@ -17,8 +18,15 @@
     }
 
     function remove(index) {
-        todos.splice(index, 1);
+        const deletedTodo = todos.splice(index, 1)[0];
+        deletedTodos.push(deletedTodo);
         todos = todos;
+        saveTodoList(todos);
+    }
+
+    function clearAllTodos() {
+        todos = [];
+        saveTodoList(todos);
     }
 
 </script>
@@ -29,7 +37,7 @@
 <input type="text" class="todo-input" bind:value={todoText}/>
 
 <!--button add-->
-<button on:click={addTodo}>ADD</button>
+<button id="add" on:click={addTodo}>ADD</button>
 
 <!--todo-->
 {#each todos as todo, index}
@@ -39,9 +47,45 @@
         <button class="delete" on:click={remove}>X</button>
     </div>
 {/each}
+
+<button on:click={clearAllTodos}>Clear All</button>
+
+
+<h2>Deleted Items</h2>
+<!--deleted todos: Eigentlich sollten hier die gelöschten todos aufgelistet werden, aber das klappt irgendwie nicht:/-->
+{#each deletedTodos as todo}
+    <div class="deleted-entry">
+        <div>{todo.text}</div>
+    </div>
+{/each}
 <!--text-->
 <!--checkboxen-->
 <style>
+    h1, h2, .todo-entry{
+        font-family: sans-serif
+    }
+
+    .todo-input {
+        padding: 10px;
+        margin-bottom: 10px;
+        font-size: 16px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+    }
+
+    .todo-entry {
+        width: 10%;
+        background-color: #fff;
+        padding: 10px;
+        margin-bottom: 10px;
+        border-radius: 4px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .todo-entry.done {
+        opacity: 0.5;
+    }
+
     .delete {
         background-color: white;
         border: none;
@@ -50,10 +94,18 @@
     .delete:hover {
         background-color: orange;
     }
+
+    .delete:active {
+        background-color: red;
+    }
     .done {
         color: grey;
     }
     .todo-entry {
         display: flex;
     }
+    .deleted-entry {
+        color: red;
+    }
+    
 </style>
